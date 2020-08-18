4f17a540838777de47a70f686653fd92150e82e3
Joshua Achorn <joshuaachorn@Joshuas-MBP.attlocal.net>
2020-08-18 16:59:12 -0700

    cleaned up files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./App.css";
-import CampingList from "./components/CampingList/CampingList";
 
 function App() {
   return (
     <div>
       <h1>Camping logger</h1>
-      <CampingList />
     </div>
   );
 }
