73077362d27d13ed00a7cac241452d3645c26c82
Joshua Achorn <joshuaachorn@Joshuas-MBP.attlocal.net>
2020-08-19 11:29:43 -0700

    added react-bootstrap styling to app file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import PageLayout from "./components/PageLayout";
 import "./App.css";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div>
-      <h1>Camping logger</h1>
+      <PageLayout />
     </div>
   );
 }
